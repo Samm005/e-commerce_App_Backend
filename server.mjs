b5e81a600abdd8baba369f48651b62f46cbdbdde
Samm005 <samudyathakaranth@gmail.com>
2026-01-27 23:05:42 +0530

    Added Mongodb connection

--- server.mjs
+++ server.mjs
@@ -1,8 +1,15 @@
 import express from 'express';
+import dotenv from 'dotenv';
 
 const app = express();
 
+dotenv.config();
+
+import db from "./config/db.mjs";
+
 app.use(express.json());
+
+db();
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000');
