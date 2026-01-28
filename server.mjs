3faef6f954a3d61261fba945fadaaee6afde5fc9
Samm005 <samudyathakaranth@gmail.com>
2026-01-28 15:00:47 +0530

    Product routes to get all products and to get product by id

--- server.mjs
+++ server.mjs
@@ -4,18 +4,21 @@ dotenv.config();
 import express from "express";
 import db from "./config/db.mjs";
 import authRoutes from "./routes/authRoutes.mjs";
+import productRoutes from "./routes/productRoutes.mjs";
 
 const app = express();
 
 app.use(express.json());
 
 app.get("/ping", (req, res) => {
-  res.send("pong");
+  res.send("pinged");
 });
 
 await db();
 
 app.use("/api/auth", authRoutes);
+app.use("/api/products", productRoutes);
+app.use("/api/products", productRoutes);
 
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
